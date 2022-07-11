1b2aedcd58792bc093cefda9ea721ee9411788d4
jatannoury <josephtannoury@outlook.com>
2022-07-11 23:07:03 +0300

    ReactNative | add: Add email text input | SignUp Screen

--- FortunusVision-FrontEnd/App.js
+++ FortunusVision-FrontEnd/App.js
@@ -1,5 +1,12 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, ImageBackground } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ImageBackground,
+  TextInput,
+} from "react-native";
+
 import SignIn from "./screens/SignIn";
 
 export default function App() {
@@ -13,6 +20,9 @@ export default function App() {
         <StatusBar backgroundColor="#5B2E0F" />
         <View>
           <Text style={styles.title}>Register</Text>
+        </View>
+        <View style={styles.form}>
+          <TextInput placeholder="E-mail" style={styles.input} />
         </View>
       </ImageBackground>
     </View>
@@ -32,5 +42,16 @@ const styles = StyleSheet.create({
     textAlign: "center",
     paddingTop: 100,
     color: "#5B2E0F",
+  },
+  form: {
+    justifyContent: "center",
+    marginHorizontal: 30,
+  },
+  input: {
+    borderBottomColor: "#5B2E0F",
+    borderBottomWidth: 3,
+    textAlign: "center",
+    color: "#5B2E0F",
+    fontWeight: "bold",
   },
 });
