ea4bbdaccccf057ca7b6889ade0d43fbc9b3c123
jatannoury <josephtannoury@outlook.com>
2022-07-12 13:15:08 +0300

    ReactNative | nesting drawer navigation to stack | Landing Screen

--- FortunusVision-FrontEnd/App.js
+++ FortunusVision-FrontEnd/App.js
@@ -2,10 +2,38 @@ import SignUpScreen from "./screens/SignUpScreen";
 import SignInScreen from "./screens/SignInScreen";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 import LandingScreen from "./screens/LandingScreen";
 import Colors from "./constants/colors";
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
 export default function App() {
+  function DrawerNavigator() {
+    return (
+      <Drawer.Navigator
+        screenOptions={{
+          headerStyle: { backgroundColor: Colors.primary500 },
+          headerTintColor: Colors.primary600,
+          sceneContainerStyle: { backgroundColor: Colors.primary500 },
+          drawerContentStyle: { backgroundColor: Colors.primary500 },
+          drawerInactiveTintColor: "#e4baa1",
+          drawerActiveTintColor: Colors.primary600,
+        }}
+      >
+        <Drawer.Screen
+          name="Landing Page"
+          component={LandingScreen}
+          options={{
+            title: "Pick Your Expert",
+            // drawerIcon: ({ color, size }) => (
+            //   <MaterialCommunityIcons name="crystal-ball" color={color} size={30} />
+            // ),
+          }}
+        />
+      </Drawer.Navigator>
+    );
+  }
+
   return (
     <NavigationContainer>
       <Stack.Navigator
@@ -32,9 +60,9 @@ export default function App() {
         />
         <Stack.Screen
           name="LandingPage"
-          component={LandingScreen}
+          component={DrawerNavigator}
           options={{
-            title: "Land",
+            title: "Landing Page",
             headerShown: false,
           }}
         />
