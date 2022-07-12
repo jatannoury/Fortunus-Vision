af1f9239a16f4b57bee24760030381df730465ce
jatannoury <josephtannoury@outlook.com>
2022-07-12 13:02:56 +0300

    ReactNative | add: Add background image | Landing Screen

--- FortunusVision-FrontEnd/screens/LandingScreen.js
+++ FortunusVision-FrontEnd/screens/LandingScreen.js
@@ -1,11 +1,23 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, ImageBackground, StyleSheet } from "react-native";
 const LandingScreen = () => {
   return (
-    <View>
-      <Text>YOOOOOOOOOOO</Text>
+    <View style={styles.container}>
+      <ImageBackground
+        source={require("../assets/backgroundImage.jpg")}
+        resizeMode="cover"
+        style={styles.image}
+      ></ImageBackground>
     </View>
   );
 };
 
 export default LandingScreen;
+const styles = StyleSheet.create({
+  image: {
+    flex: 1,
+  },
+  container: {
+    flex: 1,
+  },
+});
