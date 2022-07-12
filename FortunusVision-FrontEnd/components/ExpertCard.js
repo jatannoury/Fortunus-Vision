c35a6e44fc5a0e567a022598d1cdcadd669f5f5b
jatannoury <josephtannoury@outlook.com>
2022-07-12 22:08:57 +0300

    ReactNative | fea: edit expert card component to accept props as data

--- FortunusVision-FrontEnd/components/ExpertCard.js
+++ FortunusVision-FrontEnd/components/ExpertCard.js
@@ -3,17 +3,17 @@ import React from "react";
 import { View, Text, ImageBackground, StyleSheet, Image } from "react-native";
 import EmptyCard from "../components/EmptyCard";
 import Colors from "../constants/colors";
-const ExpertCard = () => {
+const ExpertCard = ({ name, years, age, rating, price }) => {
   return (
     <EmptyCard>
       <View>
         <View style={styles.textContainer}>
-          <Text style={styles.text}>Jamil</Text>
+          <Text style={styles.text}>{name}</Text>
         </View>
-        <Text style={styles.infoContainer}>10 years of experience</Text>
-        <Text style={styles.infoContainerOne}>85 Years Old</Text>
+        <Text style={styles.infoContainer}>{years} years of experience</Text>
+        <Text style={styles.infoContainerOne}>{age} Years Old</Text>
         <Text style={styles.infoContainerTwo}>
-          4.5 <Ionicons name="star" />
+          {rating} <Ionicons name="star" />
         </Text>
       </View>
       <View>
@@ -21,10 +21,61 @@ const ExpertCard = () => {
           source={require("../assets/Container.jpg")}
           style={styles.priceContainer}
         ></Image>
-        <Text style={[styles.price]}>100$</Text>
+        <Text style={[styles.price]}>{price}$</Text>
       </View>
     </EmptyCard>
   );
 };
 
 export default ExpertCard;
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 30,
+    fontWeight: "bold",
+    color: "white",
+    paddingBottom: 10,
+  },
+  textContainer: {
+    position: "absolute",
+    top: 80,
+    left: 435,
+    borderBottomColor: "white",
+    borderBottomWidth: 1,
+    width: 200,
+  },
+  infoContainer: {
+    position: "absolute",
+    top: 135,
+    left: 435,
+    fontSize: 15,
+    color: "white",
+  },
+  infoContainerOne: {
+    position: "absolute",
+    top: 155,
+    left: 435,
+    fontSize: 15,
+    color: "white",
+  },
+  infoContainerTwo: {
+    position: "absolute",
+    top: 175,
+    left: 435,
+    fontSize: 15,
+    color: "white",
+  },
+  priceContainer: {
+    position: "absolute",
+    top: 60,
+    left: 640,
+    flex: 1,
+  },
+  price: {
+    position: "absolute",
+    top: 72,
+    left: 652,
+    fontSize: 23,
+    fontWeight: "bold",
+    color: "white",
+  },
+});
--- FortunusVision-FrontEnd/screens/LandingScreen.js
+++ FortunusVision-FrontEnd/screens/LandingScreen.js
@@ -4,58 +4,9 @@ import { View, Text, ImageBackground, StyleSheet, Image } from "react-native";
 import ExpertCard from "../components/ExpertCard";
 import Colors from "../constants/colors";
 const LandingScreen = () => {
-  return <ExpertCard />;
+  return (
+    <ExpertCard name="Jamil" years="10" price="100" age="35" rating="4.5" />
+  );
 };
 
 export default LandingScreen;
-const styles = {
-  text: {
-    fontSize: 30,
-    fontWeight: "bold",
-    color: "white",
-    paddingBottom: 10,
-  },
-  textContainer: {
-    position: "absolute",
-    top: 80,
-    left: 435,
-    borderBottomColor: "white",
-    borderBottomWidth: 1,
-    width: 200,
-  },
-  infoContainer: {
-    position: "absolute",
-    top: 135,
-    left: 435,
-    fontSize: 15,
-    color: "white",
-  },
-  infoContainerOne: {
-    position: "absolute",
-    top: 155,
-    left: 435,
-    fontSize: 15,
-    color: "white",
-  },
-  infoContainerTwo: {
-    position: "absolute",
-    top: 175,
-    left: 435,
-    fontSize: 15,
-    color: "white",
-  },
-  priceContainer: {
-    position: "absolute",
-    top: 60,
-    left: 640,
-    flex: 1,
-  },
-  price: {
-    position: "absolute",
-    top: 72,
-    left: 652,
-    fontSize: 23,
-    fontWeight: "bold",
-    color: "white",
-  },
-};
