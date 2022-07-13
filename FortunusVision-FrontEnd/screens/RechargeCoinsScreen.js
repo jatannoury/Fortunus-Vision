d74d04b3b4b60ea6ef19576e6338bd93525b61ae
jatannoury <josephtannoury@outlook.com>
2022-07-14 01:27:53 +0300

    ReactNative | add card's coins button | Recharge Coins Screen

--- FortunusVision-FrontEnd/screens/RechargeCoinsScreen.js
+++ FortunusVision-FrontEnd/screens/RechargeCoinsScreen.js
@@ -1,5 +1,11 @@
 import React from "react";
-import { Text, View, ImageBackground, StyleSheet, Image } from "react-native";
+import {
+  Text,
+  View,
+  ImageBackground,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
 import Colors from "../constants/colors";
 const RechargeCoinsScreen = () => {
   return (
@@ -11,6 +17,9 @@ const RechargeCoinsScreen = () => {
       <View style={styles.container}>
         <Text style={styles.coinsQuantity}>100 coins</Text>
         <Text style={styles.coinsPrice}>10 $</Text>
+        <TouchableOpacity style={styles.buttonContainer}>
+          <Text style={styles.buttonContent}>Add to cart</Text>
+        </TouchableOpacity>
       </View>
     </ImageBackground>
   );
@@ -34,6 +43,21 @@ const styles = StyleSheet.create({
   coinsPrice: {
     fontSize: 70,
     fontWeight: "900",
+    color: "white",
+  },
+  button: {
+    width: 40,
+  },
+  buttonContainer: {
+    height: 40,
+    width: 145,
+    marginTop: 10,
+    backgroundColor: Colors.primary500,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  buttonContent: {
+    fontSize: 20,
     color: "white",
   },
 });
