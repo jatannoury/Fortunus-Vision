d31f96d45cf99a7a38752fbbd7a8756730765991
jatannoury <josephtannoury@outlook.com>
2022-07-13 21:06:38 +0300

    ReactNative | add: Add background image History Screen

--- FortunusVision-FrontEnd/screens/ChatsScreen.js
+++ FortunusVision-FrontEnd/screens/ChatsScreen.js
@@ -1,11 +1,20 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, ImageBackground } from "react-native";
 const ChatsScreen = () => {
   return (
-    <View>
-      <Text>YOWWWWWWW</Text>
+    <View style={styles.image}>
+      <ImageBackground
+        source={require("../assets/backgroundImage.jpg")}
+        resizeMode="cover"
+        style={styles.image}
+      ></ImageBackground>
     </View>
   );
 };
 
 export default ChatsScreen;
+const styles = StyleSheet.create({
+  image: {
+    flex: 1,
+  },
+});
