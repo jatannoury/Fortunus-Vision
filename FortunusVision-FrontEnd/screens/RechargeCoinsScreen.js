297efd82c77d560929261d7df206edc75a5359ff
jatannoury <josephtannoury@outlook.com>
2022-07-14 09:49:58 +0300

    ReactNative | add: styling most popular recharge coins card | Recharge coins screen

--- FortunusVision-FrontEnd/screens/RechargeCoinsScreen.js
+++ FortunusVision-FrontEnd/screens/RechargeCoinsScreen.js
@@ -14,12 +14,37 @@ const RechargeCoinsScreen = () => {
       resizeMode="cover"
       style={styles.image}
     >
-      <View style={styles.container}>
-        <Text style={styles.coinsQuantity}>100 coins</Text>
-        <Text style={styles.coinsPrice}>10 $</Text>
-        <TouchableOpacity style={styles.buttonContainer}>
-          <Text style={styles.buttonContent}>Add to cart</Text>
-        </TouchableOpacity>
+      <View style={styles.flexContainer}>
+        <View style={styles.container}>
+          <Text style={styles.coinsQuantity}>100 coins</Text>
+          <Text style={styles.coinsPrice}>10 $</Text>
+          <TouchableOpacity style={styles.buttonContainer}>
+            <Text style={styles.buttonContent}>Add to cart</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.container}>
+          <Text style={styles.premiumCoinsQuantity}>200 coins</Text>
+          <Text style={styles.coinsPrice}>15 $</Text>
+          <TouchableOpacity style={styles.premiumButtonContainer}>
+            <Text style={styles.buttonContent}>Add to cart</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+      <View style={styles.flexContainer}>
+        <View style={styles.container}>
+          <Text style={styles.coinsQuantity}>300 coins</Text>
+          <Text style={styles.coinsPrice}>20 $</Text>
+          <TouchableOpacity style={styles.buttonContainer}>
+            <Text style={styles.buttonContent}>Add to cart</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.container}>
+          <Text style={styles.coinsQuantity}>400 coins</Text>
+          <Text style={styles.coinsPrice}>25 $</Text>
+          <TouchableOpacity style={styles.buttonContainer}>
+            <Text style={styles.buttonContent}>Add to cart</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </ImageBackground>
   );
@@ -41,6 +66,22 @@ const styles = StyleSheet.create({
     color: "white",
     fontSize: 20,
   },
+  premiumCoinsQuantity: {
+    color: "white",
+    backgroundColor: "#C16126",
+    width: "100%",
+    textAlign: "center",
+    fontSize: 20,
+    paddingVertical: 17,
+  },
+  premiumButtonContainer: {
+    height: 40,
+    width: 145,
+    marginTop: 10,
+    backgroundColor: "#C16126",
+    justifyContent: "center",
+    alignItems: "center",
+  },
   coinsPrice: {
     fontSize: 70,
     fontWeight: "900",
@@ -61,5 +102,10 @@ const styles = StyleSheet.create({
   buttonContent: {
     fontSize: 20,
     color: "white",
+  },
+  flexContainer: {
+    marginTop: 50,
+    flexDirection: "row",
+    justifyContent: "space-around",
   },
 });
