cd44b47ff982b2fbf2fc6726926fa51cfa786671
jatannoury <josephtannoury@outlook.com>
2022-07-15 16:20:24 +0300

    NodeJs | add : Add controller folder

--- fortunusVision-Backend/src/user/index.js
+++ fortunusVision-Backend/src/user/index.js
@@ -1,5 +1,8 @@
 const { Router } = require("express");
+const { register } = require("./controller");
 const router = Router();
 // const testMiddleware = require("../../middleware/test");
+
+router.post("/register", register);
 
 module.exports = router;
