4471af4143843b315d5eac222fb201d773f587b6
jatannoury <josephtannoury@outlook.com>
2022-07-16 20:22:31 +0300

    NodeJs |  add: Add getExperts api route

--- fortunusVision-Backend/src/v1/user/index.js
+++ fortunusVision-Backend/src/v1/user/index.js
@@ -1,8 +1,9 @@
 const { Router } = require("express");
-const { register, signIn } = require("./controller");
+const { register, signIn, getExperts } = require("./controller");
 const router = Router();
 // const testMiddleware = require("../../middleware/test");
 
 router.post("/register", register);
 router.post("/signIn", signIn);
+router.get("/getExperts", getExperts);
 module.exports = router;
