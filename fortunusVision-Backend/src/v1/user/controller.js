6ce6a9f9dda17bd8ff32e7f71505d1f178edfda2
jatannoury <josephtannoury@outlook.com>
2022-07-16 04:54:32 +0300

    NodeJs | add: add register API and connecting posting into users schema

--- fortunusVision-Backend/src/v1/user/controller.js
+++ fortunusVision-Backend/src/v1/user/controller.js
@@ -1,3 +1,17 @@
 const User = require("../../../models/User");
-const {} = require("./services");
-module.exports = {};
+const { addUser } = require("./services");
+const bcrypt = require("bcryptjs");
+
+async function register(req, res) {
+  try {
+    //encrypting password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    //adding inputs to mongodb
+    const addUserResult = await addUser(req.body, hashedPassword);
+    return res.send({ user: addUserResult._id });
+  } catch (error) {
+    console.log(error);
+  }
+}
+module.exports = { register };
