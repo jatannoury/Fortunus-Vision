82feff0bf3bd59159b25b9b9cf091b7466a70399
jatannoury <josephtannoury@outlook.com>
2022-07-16 07:40:19 +0300

    ReactNative | fea: Linking using axios | signUp screen

--- FortunusVision-FrontEnd/utils/http.js
+++ FortunusVision-FrontEnd/utils/http.js
@@ -3,4 +3,29 @@ const hostname = "192.168.100.192";
 const portname = 3001;
 const BACKEND_URL = `http://${hostname}:${portname}/api/v1`;
 axios.defaults.baseURL = BACKEND_URL;
-export function registerUser(email, password) {}
+import { useState } from "react";
+let data;
+export async function registerUser(email, password, username) {
+  console.log("In http", email, password, username);
+  const response = await axios({
+    method: "post",
+    url: "/user/register",
+    data: {
+      email: email,
+      password: password,
+      userName: username,
+    },
+  }).then(
+    (response) => {
+      data = response.data;
+      response.data;
+    },
+
+    (error) => {
+      data = error;
+      throw error;
+    }
+  );
+
+  return data;
+}
