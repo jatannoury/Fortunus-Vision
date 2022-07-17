72125d811c99cae8ea1bce2c16562186e860a525
jatannoury <josephtannoury@outlook.com>
2022-07-17 13:48:12 +0300

    ReactNative | add: Add addChats reducer

--- FortunusVision-FrontEnd/redux/users.js
+++ FortunusVision-FrontEnd/redux/users.js
@@ -19,10 +19,14 @@ const usersSlice = createSlice({
     addExperts: (state, action) => {
       state.experts = action.payload;
     },
+    addChats: (state, action) => {
+      state.chats.push(action.payload);
+    },
   },
 });
 
 export const addCoins = usersSlice.actions.addCoins;
 export const addName = usersSlice.actions.addName;
 export const addExperts = usersSlice.actions.addExperts;
+export const addChats = usersSlice.actions.addChats;
 export default usersSlice.reducer;
