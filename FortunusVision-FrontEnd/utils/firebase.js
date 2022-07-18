a67dfd80ea6a865a636e86140c8d75a00a6a88f8
jatannoury <josephtannoury@outlook.com>
2022-07-18 04:18:36 +0300

    ReactNative | fea: Add axios link to firebase | Chat Screen

--- FortunusVision-FrontEnd/utils/firebase.js
+++ FortunusVision-FrontEnd/utils/firebase.js
@@ -3,3 +3,17 @@ const BACKEND_URL = "https://fortunusvision-default-rtdb.firebaseio.com/";
 export function storeVoice(infos, expert_id) {
   axios.post(BACKEND_URL + `${expert_id}.json`, infos);
 }
+export async function fetchInfo(expertId) {
+  const res = await axios.get(BACKEND_URL + `${expertId}.json`);
+  const info = [];
+  for (const key in res.data) {
+    const singleInfo = {
+      duration: res.data[key].duration,
+      date: res.data[key].date,
+      sound: res.data[key].sound,
+      usertype: res.data[key].usertype,
+    };
+    info.push(singleInfo);
+  }
+  return info;
+}
