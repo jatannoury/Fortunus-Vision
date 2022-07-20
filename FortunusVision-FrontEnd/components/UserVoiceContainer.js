52a9080c019be2a73c7b3ae80ff16cd38d76d895
jatannoury <josephtannoury@outlook.com>
2022-07-20 17:23:17 +0300

    ReactNative | fea: Display fetched pre-recorded voice messages | Chat Screen

--- FortunusVision-FrontEnd/components/UserVoiceContainer.js
+++ FortunusVision-FrontEnd/components/UserVoiceContainer.js
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FontAwesome } from "@expo/vector-icons";
+import { AntDesign } from "@expo/vector-icons";
 import { View, Text, ImageBackground, StyleSheet } from "react-native";
 import Colors from "../constants/colors";
-const UserVoiceContainer = ({ props, date, time, name }) => {
+import { Audio } from "expo-av";
+import { firebase } from "../config";
+const UserVoiceContainer = ({ sound, date, time, name }) => {
+  const soundObject = new Audio.Sound();
+  useEffect(() => {
+    async function load() {
+      await soundObject.loadAsync({ uri: sound });
+    }
+    load();
+  });
+  const [pressed, isPressed] = useState(false);
   return (
     <View style={styles.userContainer}>
       <View>
@@ -11,12 +22,33 @@ const UserVoiceContainer = ({ props, date, time, name }) => {
         </Text>
       </View>
       <View style={styles.voiceContainer}>
-        <FontAwesome
-          name="play"
-          size={30}
-          color="#D9D9D9"
-          style={styles.playButton}
-        />
+        {!pressed ? (
+          <FontAwesome
+            name="play"
+            size={30}
+            color="#D9D9D9"
+            style={styles.playButton}
+            onPress={async () => {
+              // The rest of this plays the audio
+              try {
+                await soundObject.playAsync();
+                isPressed(true);
+              } catch (error) {
+                console.log("error:", error);
+              }
+            }}
+          />
+        ) : (
+          <AntDesign
+            name="pause"
+            size={30}
+            color="#D9D9D9"
+            onPress={async () => {
+              const status = await soundObject.pauseAsync();
+              isPressed(false);
+            }}
+          />
+        )}
       </View>
     </View>
   );
@@ -27,6 +59,7 @@ const styles = StyleSheet.create({
   userContainer: {
     alignItems: "flex-end",
     marginHorizontal: 15,
+    marginVertical: 15,
   },
 
   date: {
