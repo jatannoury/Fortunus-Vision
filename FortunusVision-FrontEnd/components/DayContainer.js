3ee53aeac8c297c5d1ed7852c7b522c331d836fb
jatannoury <josephtannoury@outlook.com>
2022-07-21 01:20:47 +0300

    ReactNative | add: Add Day Container component

---
+++ FortunusVision-FrontEnd/components/DayContainer.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { StyleSheet, View, Text, Pressable } from "react-native";
+import Colors from "../constants/colors";
+const DayContainer = ({ day }) => {
+  return (
+    <View style={styles.dayContainer}>
+      <Pressable android_ripple={{ color: "white" }} style={styles.press}>
+        <Text>{day}</Text>
+      </Pressable>
+    </View>
+  );
+};
+
+export default DayContainer;
+const styles = StyleSheet.create({
+  press: {
+    backgroundColor: Colors.primary600,
+    height: 35,
+    width: 35,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  dayContainer: {
+    borderRadius: 5,
+
+    marginHorizontal: 10,
+    overflow: "hidden",
+  },
+});
