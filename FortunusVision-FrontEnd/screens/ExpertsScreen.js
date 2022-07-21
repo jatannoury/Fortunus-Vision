ac7cc659679731a908ae12b41e2ee8db66d2894b
jatannoury <josephtannoury@outlook.com>
2022-07-21 13:03:10 +0300

    ReactNative | refactor: Refactor ExpertsScreen

---
+++ FortunusVision-FrontEnd/screens/ExpertsScreen.js
@@ -0,0 +1,64 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { FontAwesome } from "@expo/vector-icons";
+import Colors from "../constants/colors";
+const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+import ChatsScreen from "./ChatsScreen";
+export function ExpertsScreen() {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: Colors.primary500 },
+        headerTintColor: Colors.primary600,
+      }}
+    >
+      <Stack.Screen
+        name="Chats"
+        component={ExpertDrawerNavigator}
+        options={{
+          title: "Chats",
+          headerShown: false,
+        }}
+      />
+    </Stack.Navigator>
+  );
+}
+
+export function ExpertDrawerNavigator() {
+  return (
+    <Drawer.Navigator
+      drawerContent={(props) => <CustomDrawer {...props} />}
+      screenOptions={{
+        headerStyle: { backgroundColor: Colors.primary500 },
+        headerTintColor: Colors.primary600,
+        sceneContainerStyle: { backgroundColor: Colors.primary500 },
+        drawerContentStyle: { backgroundColor: Colors.primary500 },
+        drawerInactiveTintColor: Colors.primary800,
+        drawerActiveTintColor: Colors.primary600,
+        drawerStyle: { backgroundColor: Colors.primary500 },
+      }}
+    >
+      <Drawer.Screen
+        name="Chats History"
+        component={ChatsScreen}
+        options={{
+          title: "Chats",
+          drawerIcon: ({ color, size }) => (
+            <FontAwesome name="wechat" color={color} size={30} />
+          ),
+        }}
+      />
+    </Drawer.Navigator>
+  );
+}
+
+export function CustomDrawer(props) {
+  return (
+    <DrawerContentScrollView {...props}>
+      <DrawerHeader name={"Joseph Tannoury"} />
+
+      <DrawerItemList {...props} />
+    </DrawerContentScrollView>
+  );
+}
