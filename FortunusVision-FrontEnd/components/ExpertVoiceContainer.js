1c5fb588d60179eea68121ff93fcc62075a1f114
jatannoury <josephtannoury@outlook.com>
2022-07-22 02:44:49 +0300

    ReactNative | fea: Add audio playability | Expert Voice Container

--- FortunusVision-FrontEnd/components/ExpertVoiceContainer.js
+++ FortunusVision-FrontEnd/components/ExpertVoiceContainer.js
@@ -2,23 +2,56 @@ import React from "react";
 import { FontAwesome } from "@expo/vector-icons";
 import { View, Text, ImageBackground, StyleSheet } from "react-native";
 import Colors from "../constants/colors";
+import { Audio } from "expo-av";
+import { useSelector, useDispatch } from "react-redux";
+import { useState } from "react";
+import { AntDesign } from "@expo/vector-icons";
+const ExpertVoiceContainer = ({ sound, date, time, props }) => {
+  const name = useSelector((state) => state.user);
+  console.log(name);
+  const soundObject = new Audio.Sound();
+  async function load() {
+    await soundObject.loadAsync({ uri: sound });
+  }
+  load();
 
-const ExpertVoiceContainer = ({ props, date, time }) => {
+  const [pressed, isPressed] = useState(false);
   return (
     <View style={styles.expertContainer}>
       <Text style={styles.name}>
-        {`${props.name} `}{" "}
+        {`${props} `}{" "}
         <Text style={styles.date}>
           {time}, {date}
         </Text>
       </Text>
       <View style={styles.voiceContainer}>
-        <FontAwesome
-          name="play"
-          size={30}
-          color="#D9D9D9"
-          style={styles.playButton}
-        />
+        {!pressed ? (
+          <FontAwesome
+            name="play"
+            size={30}
+            color="#D9D9D9"
+            style={styles.playButton}
+            onPress={async () => {
+              // The rest of this plays the audio
+              try {
+                await soundObject.playAsync();
+                isPressed(true);
+              } catch (error) {
+                console.log("error:", error);
+              }
+            }}
+          />
+        ) : (
+          <AntDesign
+            name="pause"
+            size={30}
+            color="#D9D9D9"
+            onPress={async () => {
+              const status = await soundObject.pauseAsync();
+              isPressed(false);
+            }}
+          />
+        )}
       </View>
     </View>
   );
