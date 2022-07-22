abbc7cd22a18fbb93ac916338e216016ca2022f3
jatannoury <josephtannoury@outlook.com>
2022-07-22 22:25:23 +0300

    ReactNative | add: Style main container | Users Booking Screen

--- FortunusVision-FrontEnd/screens/UsersDatesAndTimes.js
+++ FortunusVision-FrontEnd/screens/UsersDatesAndTimes.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { View, StyleSheet, Text } from "react-native";
 import EmptyCard from "../components/EmptyCard";
+import Colors from "../constants/colors";
 const UsersDatesAndTimes = () => {
   return (
     <View>
@@ -14,4 +15,27 @@ const UsersDatesAndTimes = () => {
 };
 
 export default UsersDatesAndTimes;
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  card: {
+    height: "100%",
+    width: "130%",
+    alignItems: "center",
+    position: "relative",
+  },
+  container: {
+    height: 600,
+    borderRadius: 25,
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 30,
+    marginVertical: 20,
+    fontWeight: "bold",
+    color: Colors.primary600,
+  },
+
+  flex: {
+    flexDirection: "row",
+    marginVertical: 15,
+  },
+});
