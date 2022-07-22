649c477fa5534e5892d50c7962685ca5a88c6957
jatannoury <josephtannoury@outlook.com>
2022-07-22 14:59:55 +0300

    ReactNative | add: Add Time Display component | Booking Screen

--- FortunusVision-FrontEnd/screens/BookingScreen.js
+++ FortunusVision-FrontEnd/screens/BookingScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, FlatList, ImageBackground, StyleSheet } from "react-native";
+import { View, Text, ImageBackground, StyleSheet } from "react-native";
 import DatesAndTimes from "../components/DatesAndTimes";
 const BookingScreen = () => {
   return (
