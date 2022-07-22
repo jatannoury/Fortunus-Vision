e6e434e71235f7922625bc81ed888b26b5ec5730
jatannoury <josephtannoury@outlook.com>
2022-07-23 00:06:56 +0300

    ReactNative | add: Display expert's availability | Booking Screen

--- FortunusVision-FrontEnd/screens/UsersDatesAndTimes.js
+++ FortunusVision-FrontEnd/screens/UsersDatesAndTimes.js
@@ -1,24 +1,28 @@
 import React, { useEffect, useState } from "react";
-import { View, StyleSheet, Text } from "react-native";
+import { View, StyleSheet, Text, ScrollView } from "react-native";
+import DayContainer from "../components/DayContainer";
 import EmptyCard from "../components/EmptyCard";
+import DayDisplay from "../components/DayDisplay";
 import Colors from "../constants/colors";
 import { getAppointment } from "../utils/http";
 const UsersDatesAndTimes = ({ expert_id }) => {
   const [availabality, setAvailabiliy] = useState([]);
-  console.log(expert_id);
 
-  async function fetchAppointments() {
-    console.log(expert_id);
-    let res = await getAppointment(expert_id);
-    console.log(res);
-  }
-  fetchAppointments();
+  useEffect(() => {
+    async function fetchAppointments() {
+      console.log(expert_id);
+      let res = await getAppointment(expert_id);
+      setAvailabiliy(res);
+    }
+    fetchAppointments();
+  }, []);
 
   return (
     <View>
       <View style={styles.flex}>
         <EmptyCard style={styles.container} card={styles.card}>
           <Text style={styles.text}> Choose from the below</Text>
+          <DayDisplay availabality={availabality} />
         </EmptyCard>
       </View>
     </View>
