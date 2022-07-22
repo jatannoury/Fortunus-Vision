4bf9b374d77a4bce2073bfec02b727a8d7cf5174
jatannoury <josephtannoury@outlook.com>
2022-07-22 22:16:01 +0300

    ReactNative | mod: separate rendered component depending on userType | Booking Screen

--- FortunusVision-FrontEnd/screens/BookingScreen.js
+++ FortunusVision-FrontEnd/screens/BookingScreen.js
@@ -1,16 +1,19 @@
 import React from "react";
 import { View, Text, ImageBackground, StyleSheet } from "react-native";
-import DatesAndTimes from "../components/DatesAndTimes";
+import ExpertsDatesAndTimes from "../components/ExpertsDatesAndTimes";
+import { useSelector } from "react-redux";
+import UsersDatesAndTimes from "./UsersDatesAndTimes";
 const BookingScreen = () => {
+  const userType = useSelector((state) => state.user.userType);
+
   return (
     <ImageBackground
       source={require("../assets/backgroundImage.jpg")}
       resizeMode="cover"
       style={styles.image}
     >
-      <View>
-        <DatesAndTimes />
-      </View>
+      <View>{userType === 1 && <ExpertsDatesAndTimes />}</View>
+      <View>{userType === 0 && <UsersDatesAndTimes />}</View>
     </ImageBackground>
   );
 };
