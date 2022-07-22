d7f71f0a9ab6a5e7fd1d7fee584b8b694d6a4d6c
jatannoury <josephtannoury@outlook.com>
2022-07-22 15:44:28 +0300

    ReactNative | add: Style submit button | Booking Screen

--- FortunusVision-FrontEnd/components/SubmitButt.js
+++ FortunusVision-FrontEnd/components/SubmitButt.js
@@ -2,20 +2,30 @@ import React from "react";
 import { Pressable, View, Text, StyleSheet } from "react-native";
 import Colors from "../constants/colors";
 
-const SubmitButton = () => {
+const SubmitButt = () => {
   return (
     <Pressable style={styles.rootContainer}>
-      <View>
+      <View style={styles.Container}>
         <Text style={styles.text}>Submit</Text>
       </View>
     </Pressable>
   );
 };
 
-export default SubmitButton;
+export default SubmitButt;
 const styles = StyleSheet.create({
   rootContainer: {
+    justifyContent: "center",
+    alignItems: "flex-end",
+    marginRight: 25,
+  },
+  Container: {
     backgroundColor: Colors.primary500,
+    width: "50%",
+    alignItems: "center",
+    justifyContent: "center",
+    height: 50,
+    borderRadius: 10,
   },
   text: {
     color: "white",
