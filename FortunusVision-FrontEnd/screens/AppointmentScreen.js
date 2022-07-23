470f8ac5649dd82e65fd88c230ea8a39fb12a0ff
jatannoury <josephtannoury@outlook.com>
2022-07-23 18:48:30 +0300

    ReactNative | add: Fetch expert's info | Appointment Screen

--- FortunusVision-FrontEnd/screens/AppointmentScreen.js
+++ FortunusVision-FrontEnd/screens/AppointmentScreen.js
@@ -1,72 +1,82 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 import {
   ImageBackground,
   StyleSheet,
   View,
-  Text,
   Pressable,
   FlatList,
 } from "react-native";
-import EmptyCard from "../components/EmptyCard";
 import AppointmentCard from "../components/AppointmentCard";
-import { MaterialIcons } from "@expo/vector-icons";
-import ExpertCard from "../components/ExpertCard";
+import { getExpertsById } from "../utils/http";
 const AppointmentScreen = ({ navigation }) => {
+  const [expertsInfo, setExpertsInfo] = useState([]);
+  const appointments = useSelector((state) => state.user.appointments);
+
+  useEffect(() => {
+    console.log(appointments.length);
+    appointments.map(async (appointment) => {
+      let res = await getExpertsById(appointment.expert_id);
+      setExpertsInfo([...expertsInfo, res]);
+    });
+    console.log(expertsInfo);
+  }, []);
+  console.log();
   let item = [
-    {
-      name: "Jamil",
-      waiting: "10",
-      price: "100",
-      age: "35",
-      rating: "4.5",
-      phonePrice: "50",
-      waiting: "6",
-    },
-    {
-      name: "Toni",
-      waiting: "5",
-      price: "50",
-      age: "22",
-      rating: "3.5",
-      phonePrice: "8",
-      waiting: "14",
-    },
-    {
-      name: "Joseph",
-      waiting: "7",
-      price: "70",
-      age: "22",
-      rating: "5.0",
-      phonePrice: "7",
-      waiting: "24",
-    },
-    {
-      name: "Stephanelle",
-      waiting: "4",
-      price: "55",
-      age: "25",
-      rating: "2.5",
-      phonePrice: "23",
-      waiting: "55",
-    },
-    {
-      name: "Hamz",
-      waiting: "6",
-      price: "520",
-      age: "32",
-      rating: "4.5",
-      phonePrice: "120",
-      waiting: "10",
-    },
-    {
-      name: "Mo Tahan",
-      waiting: "10",
-      price: "100",
-      age: "35",
-      rating: "4.5",
-      phonePrice: "50",
-      waiting: "100",
-    },
+    // {
+    //   name: "Jamil",
+    //   waiting: "10",
+    //   price: "100",
+    //   age: "35",
+    //   rating: "4.5",
+    //   phonePrice: "50",
+    //   waiting: "6",
+    // },
+    // {
+    //   name: "Toni",
+    //   waiting: "5",
+    //   price: "50",
+    //   age: "22",
+    //   rating: "3.5",
+    //   phonePrice: "8",
+    //   waiting: "14",
+    // },
+    // {
+    //   name: "Joseph",
+    //   waiting: "7",
+    //   price: "70",
+    //   age: "22",
+    //   rating: "5.0",
+    //   phonePrice: "7",
+    //   waiting: "24",
+    // },
+    // {
+    //   name: "Stephanelle",
+    //   waiting: "4",
+    //   price: "55",
+    //   age: "25",
+    //   rating: "2.5",
+    //   phonePrice: "23",
+    //   waiting: "55",
+    // },
+    // {
+    //   name: "Hamz",
+    //   waiting: "6",
+    //   price: "520",
+    //   age: "32",
+    //   rating: "4.5",
+    //   phonePrice: "120",
+    //   waiting: "10",
+    // },
+    // {
+    //   name: "Mo Tahan",
+    //   waiting: "10",
+    //   price: "100",
+    //   age: "35",
+    //   rating: "4.5",
+    //   phonePrice: "50",
+    //   waiting: "100",
+    // },
   ];
   function renderCardItem(itemData) {
     itemData = itemData.item;
