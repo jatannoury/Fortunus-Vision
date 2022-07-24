7596ee456d237afa17e3a6fb9cdc77b06dfacac3
jatannoury <josephtannoury@outlook.com>
2022-07-24 14:57:30 +0300

    ReactNative | add: Add profile picture | Call Screen

--- FortunusVision-FrontEnd/screens/GettingCall.js
+++ FortunusVision-FrontEnd/screens/GettingCall.js
@@ -1,7 +1,8 @@
 import { StyleSheet, Text, View, ImageBackground } from "react-native";
 import React from "react";
-import ProfilePicture from "./ProfilePicture";
+
 import Colors from "../constants/colors";
+import ProfilePicture from "./ProfilePicture";
 export default function GettingCall(props) {
   return (
     <ImageBackground
@@ -9,7 +10,12 @@ export default function GettingCall(props) {
       resizeMode="cover"
       style={styles.image}
     >
-      <View style={styles.rootImgContainer}></View>
+      <View style={styles.rootImgContainer}>
+        <ProfilePicture
+          path={require("../assets/PERSONAL_PROFILE_PIC.jpg")}
+          containerStyle={styles.imgContainer}
+        />
+      </View>
     </ImageBackground>
   );
 }
@@ -17,5 +23,15 @@ export default function GettingCall(props) {
 const styles = StyleSheet.create({
   image: {
     flex: 1,
+  },
+  imgContainer: {
+    width: 105,
+    height: 105,
+    borderRadius: 50,
+    borderWidth: 6,
+    borderColor: Colors.primary500,
+    overflow: "hidden",
+    marginRight: 20,
+    marginTop: 20,
   },
 });
