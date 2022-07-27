9e117b11d2b94d43efb72c90bd24ed3d0bd20d38
jatannoury <josephtannoury@outlook.com>
2022-07-28 01:35:16 +0300

    ReactNative | add: Render fetched Data | History Screen

--- FortunusVision-FrontEnd/screens/HistoryScreen.js
+++ FortunusVision-FrontEnd/screens/HistoryScreen.js
@@ -16,62 +16,7 @@ const HistoryScreen = ({ navigation }) => {
     fetchData();
   }, []);
   console.log(response);
-  let item = [
-    {
-      name: "Jamil",
-      waiting: "10",
-      price: "100",
-      age: "35",
-      rating: "4.5",
-      phonePrice: "50",
-      waiting: "6",
-    },
-    {
-      name: "Toni",
-      waiting: "5",
-      price: "50",
-      age: "22",
-      rating: "3.5",
-      phonePrice: "8",
-      waiting: "14",
-    },
-    {
-      name: "Joseph",
-      waiting: "7",
-      price: "70",
-      age: "22",
-      rating: "5.0",
-      phonePrice: "7",
-      waiting: "24",
-    },
-    {
-      name: "Stephanelle",
-      waiting: "4",
-      price: "55",
-      age: "25",
-      rating: "2.5",
-      phonePrice: "23",
-      waiting: "55",
-    },
-    {
-      name: "Hamz",
-      waiting: "6",
-      price: "520",
-      age: "32",
-      rating: "4.5",
-      phonePrice: "120",
-      waiting: "10",
-    },
-    {
-      name: "Mo Tahan",
-      waiting: "10",
-      price: "100",
-      age: "35",
-      rating: "4.5",
-      phonePrice: "50",
-      waiting: "100",
-    },
-  ];
+  let item = response;
   function renderCardItem(itemData) {
     itemData = itemData.item;
     const CardItemProps = {
