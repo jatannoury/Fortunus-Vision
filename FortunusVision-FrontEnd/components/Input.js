123c9b7597122375a8d38b7daaf95654c357d963
jatannoury <josephtannoury@outlook.com>
2022-07-27 11:41:14 +0300

    ReactNative | mod: Modify Input component to accepy more props

--- FortunusVision-FrontEnd/components/Input.js
+++ FortunusVision-FrontEnd/components/Input.js
@@ -1,14 +1,33 @@
 import React from "react";
-import { View, Text, StyleSheet, TextInput } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  becomeAnExpert,
+} from "react-native";
 import Colors from "../constants/colors";
-const Input = ({ label, onChangeText, invalid }) => {
+const Input = ({ label, onChangeText, invalid, becomeAnExpert }) => {
   return (
-    <View>
-      <Text style={[styles.name, invalid && styles.invalidLabel]}>{label}</Text>
+    <View style={[becomeAnExpert && styles.container]}>
+      <Text
+        style={[
+          styles.name,
+          invalid && styles.invalidLabel,
+          becomeAnExpert && styles.becomeAnExpertTitle,
+        ]}
+      >
+        {label}
+      </Text>
       <TextInput
         placeholderTextColor={Colors.primary600}
         placeholder={`${label}`}
-        style={[styles.input, invalid && styles.invalidInput]}
+        {...becomeAnExpert}
+        style={[
+          styles.input,
+          invalid && styles.invalidInput,
+          becomeAnExpert && styles.becomeAnExpert,
+        ]}
         onChangeText={onChangeText}
       />
     </View>
@@ -37,5 +56,17 @@ const styles = StyleSheet.create({
     color: "red",
     fontSize: 20,
     marginTop: 10,
+  },
+  becomeAnExpert: {
+    borderWidth: 2,
+    borderBottomColor: Colors.primary500,
+    borderRadius: 5,
+  },
+  becomeAnExpertTitle: {
+    fontSize: 15,
+  },
+  container: {
+    flex: 1,
+    padding: 5,
   },
 });
