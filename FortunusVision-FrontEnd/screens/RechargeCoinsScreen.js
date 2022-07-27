c454e4625005dbb120e185a2843a16349d4a351f
jatannoury <josephtannoury@outlook.com>
2022-07-27 10:44:36 +0300

    ReactNative | add: Add link between purchase buttons and updateCoins endpoint | RecahrgeCoinsScreen

--- FortunusVision-FrontEnd/screens/RechargeCoinsScreen.js
+++ FortunusVision-FrontEnd/screens/RechargeCoinsScreen.js
@@ -1,13 +1,44 @@
 import React from "react";
+import bundles from "../constants/bundles";
 import {
   Text,
   View,
   ImageBackground,
   StyleSheet,
   TouchableOpacity,
+  Alert,
 } from "react-native";
 import Colors from "../constants/colors";
-const RechargeCoinsScreen = () => {
+import { updateCoins } from "../utils/http";
+import { useSelector, useDispatch } from "react-redux";
+import { addCoins } from "../redux/users";
+
+const RechargeCoinsScreen = ({ navigation }) => {
+  let userCoins = useSelector((state) => state.user.coins);
+  let userId = useSelector((state) => state.user.userId);
+  let dispatch = useDispatch();
+  async function update(addedCoins, userId) {
+    console.log(addedCoins, userId);
+    let res = await updateCoins(userId, addedCoins);
+    console.log(res);
+
+    if (res.message === "Success") {
+      dispatch(addCoins(userCoins + addedCoins));
+      Alert.alert(
+        "Success",
+        `Your new balance is ${userCoins + addedCoins} coins`
+      );
+
+      navigation.goBack();
+    } else {
+      Alert.alert("error");
+    }
+  }
+  const OfferOne = bundles.offerOne;
+  const OfferTwo = bundles.offerTwo;
+  const OfferThree = bundles.offerThree;
+  const OfferFour = bundles.offerFour;
+  const coins = useSelector((state) => state.user.coins);
   return (
     <ImageBackground
       source={require("../assets/backgroundImage.jpg")}
@@ -17,32 +48,58 @@ const RechargeCoinsScreen = () => {
       <Text style={styles.title}>Don't have enough coins?</Text>
       <View style={styles.flexContainer}>
         <View style={styles.container}>
-          <Text style={styles.coinsQuantity}>100 coins</Text>
-          <Text style={styles.coinsPrice}>10 $</Text>
-          <TouchableOpacity style={styles.buttonContainer}>
+          <Text style={styles.coinsQuantity}>{OfferOne.coinsNumber} coins</Text>
+          <Text style={styles.coinsPrice}>
+            {OfferOne.coinsPrice} {OfferOne.currency}
+          </Text>
+          <TouchableOpacity
+            style={styles.buttonContainer}
+            onPress={update.bind("this", OfferOne.coinsNumber, userId)}
+          >
             <Text style={styles.buttonContent}>Add to cart</Text>
           </TouchableOpacity>
         </View>
         <View style={styles.container}>
-          <Text style={styles.premiumCoinsQuantity}>200 coins</Text>
-          <Text style={styles.coinsPrice}>15 $</Text>
-          <TouchableOpacity style={styles.premiumButtonContainer}>
+          <Text style={styles.premiumCoinsQuantity}>
+            {OfferTwo.coinsNumber} coins
+          </Text>
+          <Text style={styles.coinsPrice}>
+            {OfferTwo.coinsPrice} {OfferTwo.currency}
+          </Text>
+          <TouchableOpacity
+            style={styles.premiumButtonContainer}
+            onPress={update.bind("this", OfferTwo.coinsNumber, userId)}
+          >
             <Text style={styles.buttonContent}>Add to cart</Text>
           </TouchableOpacity>
         </View>
       </View>
       <View style={styles.flexContainer}>
         <View style={styles.container}>
-          <Text style={styles.coinsQuantity}>300 coins</Text>
-          <Text style={styles.coinsPrice}>20 $</Text>
-          <TouchableOpacity style={styles.buttonContainer}>
+          <Text style={styles.coinsQuantity}>
+            {OfferThree.coinsNumber} coins
+          </Text>
+          <Text style={styles.coinsPrice}>
+            {OfferThree.coinsPrice} {OfferThree.currency}
+          </Text>
+          <TouchableOpacity
+            style={styles.buttonContainer}
+            onPress={update.bind(this, OfferThree.coinsNumber, userId)}
+          >
             <Text style={styles.buttonContent}>Add to cart</Text>
           </TouchableOpacity>
         </View>
         <View style={styles.container}>
-          <Text style={styles.coinsQuantity}>400 coins</Text>
-          <Text style={styles.coinsPrice}>25 $</Text>
-          <TouchableOpacity style={styles.buttonContainer}>
+          <Text style={styles.coinsQuantity}>
+            {OfferFour.coinsNumber} coins
+          </Text>
+          <Text style={styles.coinsPrice}>
+            {OfferFour.coinsPrice} {OfferFour.currency}
+          </Text>
+          <TouchableOpacity
+            style={styles.buttonContainer}
+            onPress={update.bind("this", OfferFour.coinsNumber, userId)}
+          >
             <Text style={styles.buttonContent}>Add to cart</Text>
           </TouchableOpacity>
         </View>
