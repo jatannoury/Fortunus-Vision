e233621fa605eecdd6a86887e3c0b3b1b6c970c8
jatannoury <josephtannoury@outlook.com>
2022-07-28 03:02:04 +0300

    ReactNative | add: Add profile picture | Rating Screen

--- FortunusVision-FrontEnd/screens/RatingScreen.js
+++ FortunusVision-FrontEnd/screens/RatingScreen.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { View, ImageBackground, StyleSheet, Text } from "react-native";
 import Colors from "../constants/colors";
+import ProfilePicture from "./ProfilePicture";
 const RatingScreen = () => {
   return (
     <ImageBackground
@@ -11,6 +12,12 @@ const RatingScreen = () => {
       <View style={styles.rootContainer}>
         <View style={styles.titleContainer}>
           <Text style={styles.title}>Rate Your Experience</Text>
+        </View>
+        <View style={styles.imageRootContainer}>
+          <ProfilePicture
+            path={require("../assets/PERSONAL_PROFILE_PIC.jpg")}
+            containerStyle={styles.imgContainer}
+          />
         </View>
       </View>
     </ImageBackground>
@@ -24,7 +31,7 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   rootContainer: {
-    backgroundColor: "white",
+    backgroundColor: Colors.primary700,
     height: 500,
     marginHorizontal: 50,
     borderRadius: 10,
@@ -37,5 +44,18 @@ const styles = StyleSheet.create({
   titleContainer: {
     marginTop: 10,
     marginLeft: 10,
+  },
+  imgContainer: {
+    width: 105,
+    height: 105,
+    borderRadius: 50,
+    borderWidth: 6,
+    borderColor: Colors.primary500,
+    overflow: "hidden",
+    marginRight: 20,
+    marginVertical: 10,
+  },
+  imageRootContainer: {
+    alignItems: "center",
   },
 });
