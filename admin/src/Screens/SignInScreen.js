dc961b03f1f827bd486c273ab81500c1f5796d64
jatannoury <josephtannoury@outlook.com>
2022-07-30 22:39:34 +0300

    ReactJs | add: Add Email text input | Sign In Screen

--- admin/src/Screens/SignInScreen.js
+++ admin/src/Screens/SignInScreen.js
@@ -1,10 +1,23 @@
 import React from "react";
 import "../styling/SignIn.css";
 const SignInScreen = () => {
+  function handleSubmit() {}
   return (
-    <div>
-      <div className="backgroundImage">
-        <span className="Apptitle">Fortunus Vision</span>
+    <div className="backgroundImage">
+      <span className="Apptitle">Fortunus Vision</span>
+      <div class="formRootContainer">
+        <form onSubmit={handleSubmit} className="formContainer">
+          <label className="label">Email </label>
+          <input
+            placeholder="admin@outlook.com"
+            className="input"
+            type="text"
+            // value={this.state.value}
+            // onChange={this.handleChange}
+          />
+
+          {/* <input type="submit" value="Submit" /> */}
+        </form>
       </div>
     </div>
   );
