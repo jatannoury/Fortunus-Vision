b5b499823bfca97ee44ca4a04e826e13f2d5b6cc
jatannoury <josephtannoury@outlook.com>
2022-07-30 21:53:37 +0300

    ReactJs | add: Add website title | SignIn Screen

---
+++ admin/src/Screens/SignInScreen.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styling/SignIn.css";
+const SignInScreen = () => {
+  return (
+    <div>
+      <div className="backgroundImage">
+        <span className="Apptitle">Fortunus Vision</span>
+      </div>
+    </div>
+  );
+};
+
+export default SignInScreen;
