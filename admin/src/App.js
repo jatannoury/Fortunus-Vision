59068234184de17b648331943a489ec5aa21efdb
jatannoury <josephtannoury@outlook.com>
2022-07-30 21:34:46 +0300

    ReactJs | add: Add background image | SignIn screen

---
+++ admin/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import SignUpScreen from "./Screens/SignUpScreen";
+
+function App() {
+  return (
+    <div>
+      <SignUpScreen />
+    </div>
+  );
+}
+
+export default App;
---
+++ admin/src/Screens/SignUpScreen.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./SignUp.css";
+const SignUpScreen = () => {
+  return (
+    <div>
+      <div className="backgroundImage"></div>
+    </div>
+  );
+};
+
+export default SignUpScreen;
