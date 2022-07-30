41a3a25e2aa146a3d4925d8ef9580b4b209a3c87
jatannoury <josephtannoury@outlook.com>
2022-07-30 08:23:18 +0300

    ReactNative | fix: bug fixing in AgoraRTC call | Call Screen

--- FortunusVision-FrontEnd/Agora/components/AppAgora.js
+++ FortunusVision-FrontEnd/Agora/components/AppAgora.js
@@ -1,14 +1,11 @@
 import React, { useRef, useCallback, useEffect, useState } from "react";
 import RtcEngine from "react-native-agora";
 import { useDispatch, useSelector } from "react-redux";
-import {
-  addCallHistory,
-  addExperts,
-  triggerPhoneCall,
-} from "../../redux/users";
+import { triggerPhoneCall } from "../../redux/users";
 import GettingCall from "../../screens/GettingCall";
-import { addHistory, triggerCall } from "../../utils/http";
+import { triggerCall } from "../../utils/http";
 import { requestAudioPermission } from "../Permission";
+
 const AppAgora = ({ navigation, route, Name }) => {
   useEffect(() => {
     async () => {
@@ -16,33 +13,29 @@ const AppAgora = ({ navigation, route, Name }) => {
       joinChannel();
     };
   }, []);
-
+  let expert_id = userType === 0 ? route.params.expert_id : 0;
+  let phonePrice = userType === 0 ? route.params.phonePrice : 0;
   let name = Name;
   if (name === undefined) {
     name = route.params.expertName;
   }
   let userId = useSelector((state) => state.user.userId);
-  let userType = useSelector((state) => state.user.userType);
-  let experts = useSelector((state) => state.user.experts);
+  let userType = useSelector((state) => {
+    state.user.userType;
+  });
 
-  let expert_details = experts?.filter(
-    (expert) => expert.expert_id === route.params.expert_id
-  )[0];
-
-  let index = experts?.indexOf(expert_details);
-  console.log("EXPEEEERTS", index);
   let dispatch = useDispatch();
   requestAudioPermission();
   const appId = "455c2d5179604814ae6fc8fbbd65a29b";
   const token =
-    "006455c2d5179604814ae6fc8fbbd65a29bIACXT4A2PyRpO3Zp5LkRMxgFphSwWagB2jYfzeReEUg7gWWPLkIAAAAAEABUJOp9ot/iYgEAAQCj3+Ji";
+    "006455c2d5179604814ae6fc8fbbd65a29bIAD4JMnmPPtg5yKs/2KYErS7AuJbBKcwDhhrgSUIH/bEJmWPLkIAAAAAEABUJOp9+23lYgEAAQD6beVi";
   const channelName = "FortunusVision";
   const rtcEngine = useRef(null);
   const [joinSuceed, setJoinSucceed] = useState(false);
   const [peerIds, setPeerIds] = useState([]);
   const [isMute, setIsMute] = useState(false);
   const [isSpeakerEnable, setIsSpeakerEnable] = useState(false);
-  const expert_id = userType === 0 ? route.params.expert_id : 0;
+  let index = userType === 0 ? route.params.index : 0;
   const initAgora = useCallback(async () => {
     rtcEngine.current = await RtcEngine.create(appId);
     await rtcEngine.current?.enableAudio();
@@ -81,7 +74,7 @@ const AppAgora = ({ navigation, route, Name }) => {
   });
   const joinChannel = useCallback(async () => {
     console.log("JOINED");
-
+    userType === 0 ? await triggerCall(expert_id, name, phonePrice) : "";
     await rtcEngine.current?.joinChannel(token, channelName, null, 0);
   }, [channelName]);
   const leaveChannel = useCallback(async () => {
@@ -89,12 +82,10 @@ const AppAgora = ({ navigation, route, Name }) => {
     setPeerIds([]);
     setJoinSucceed(false);
     await destroyAgoraEngine();
-    userType == 0 && submit();
-    dispatch(addExperts());
+    console.log("BYEEEEE");
     dispatch(triggerPhoneCall(0));
     await triggerCall(userId, "", 0);
-
-    navigation.navigate("RatingScreen", { expert_id, index, experts });
+    navigation.navigate("RatingScreen", { expert_id, index });
   }, []);
   const toggleIsMute = useCallback(async () => {
     await rtcEngine.current?.muteLocalAudioStream(!isMute);
@@ -112,16 +103,7 @@ const AppAgora = ({ navigation, route, Name }) => {
     console.log("INITIALIZED");
     initAgora();
     rtcEngine.current?.GetEngine(appId);
-    // Starts an audio device test.
-    console.log(rtcEngine.current?.StartEchoTest(1));
-    // Stops the audio device test.
-    rtcEngine.current?.StopEchoTest();
-  }, [destroyAgoraEngine, initAgora]);
-  joinChannel();
-  async function submit() {
-    await addHistory(userId, name, price);
-    dispatch(addCallHistory([{ name: name, price: price }]));
-  }
+  }, []);
   return (
     <GettingCall
       startCall={joinChannel}
