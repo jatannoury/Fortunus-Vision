20f285a59936dfad1bdc13dadcf7daa18105ab46
jatannoury <josephtannoury@outlook.com>
2022-07-31 01:57:20 +0300

    ReactJs | add: Add axios link to signUserIn endpoint

---
+++ admin/src/helper/http.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+const BACKEND_URL = "http://192.168.1.117:3001/api/v1";
+
+axios.defaults.baseURL = BACKEND_URL;
+let data;
+export async function signUserIn(email, password) {
+  await axios({
+    method: "post",
+    url: "/user/signIn",
+    data: {
+      email: email,
+      password: password,
+    },
+  })
+    .then((response) => {
+      data = response.data;
+    })
+    .catch((error) => {
+      data = error;
+      console.log(error);
+    });
+  return data;
+}
