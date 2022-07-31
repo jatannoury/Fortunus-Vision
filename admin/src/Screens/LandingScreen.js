819bbf4201785dc2feab5ee9ffc6f19eb98b3bfd
jatannoury <josephtannoury@outlook.com>
2022-07-31 11:37:40 +0300

    ReactJs | add: Add user number container

--- admin/src/Screens/LandingScreen.js
+++ admin/src/Screens/LandingScreen.js
@@ -1,7 +1,15 @@
 import React from "react";
-
+import "../styling/LandingScreen.css";
 const LandingScreen = () => {
-  return <div className="backgroundImage"></div>;
+  return (
+    <div className="backgroundImage">
+      <div className="mainContainer">
+        <div className="box">
+          <span className="content">100 users</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LandingScreen;
