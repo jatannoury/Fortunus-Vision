033a764ca7056d985f62549296c78981487626cc
jatannoury <josephtannoury@outlook.com>
2022-08-01 00:26:03 +0300

    ReactJs | add: Add Navbar | Search Users Screen

--- admin/src/Screens/SearchUsers.js
+++ admin/src/Screens/SearchUsers.js
@@ -2,7 +2,11 @@ import React from "react";
 import NavBar from "../components/NavBar";
 
 const SearchUsers = () => {
-  return <div className="backgroundImage1"></div>;
+  return (
+    <div className="backgroundImage1">
+      <NavBar />
+    </div>
+  );
 };
 
 export default SearchUsers;
