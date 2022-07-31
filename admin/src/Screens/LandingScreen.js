66577a042db7bcd68e6ac326baaf73a8b1ba348c
jatannoury <josephtannoury@outlook.com>
2022-07-31 11:53:35 +0300

    ReactJs | refactor: Refactor box container | Landing Screen

--- admin/src/Screens/LandingScreen.js
+++ admin/src/Screens/LandingScreen.js
@@ -1,12 +1,11 @@
 import React from "react";
+import BoxInfo from "../components/BoxInfo";
 import "../styling/LandingScreen.css";
 const LandingScreen = () => {
   return (
     <div className="backgroundImage">
       <div className="mainContainer">
-        <div className="box">
-          <span className="content">100 users</span>
-        </div>
+        <BoxInfo />
       </div>
     </div>
   );
---
+++ admin/src/components/BoxInfo.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const BoxInfo = () => {
+  return (
+    <div className="box">
+      <span className="content">100 users</span>
+    </div>
+  );
+};
+
+export default BoxInfo;
