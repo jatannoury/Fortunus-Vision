358f93bda6d1e9dd4ee4f90df706913b43d50a90
jatannoury <josephtannoury@outlook.com>
2022-07-31 13:56:10 +0300

    ReactJs | add: Add admin Sections | Landing Screen

--- admin/src/components/NavBar.js
+++ admin/src/components/NavBar.js
@@ -1,13 +1,44 @@
 import React from "react";
 import { GiPointyHat } from "react-icons/gi";
+import {
+  AiOutlineUser,
+  AiOutlineUserAdd,
+  AiOutlineSearch,
+} from "react-icons/ai";
+
 const NavBar = () => {
   return (
     <div className="NavBarContainer">
+      <div className="dummy"></div>
       <div className="title">
         Fortunus Vision <GiPointyHat className="icon1" />
       </div>
-
-      <div className="sections"></div>
+      <div className="sections ">
+        <div class="dropdown">
+          <div className="center">
+            <AiOutlineSearch className="icon2 dropdown" />
+          </div>
+          <div class="dropdown-content">
+            <p>Search</p>
+          </div>
+        </div>
+        <div class="dropdown">
+          <div className="center">
+            <AiOutlineUser className=" icon2 dropdown" />
+          </div>
+          <div class="dropdown-content">
+            <p>See All Users </p>
+          </div>
+        </div>
+        <div class="dropdown">
+          <div className="center">
+            <AiOutlineUserAdd className="icon2 dropdown" />
+          </div>
+          <div class="dropdown-content">
+            <p>Accept An Expert </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
