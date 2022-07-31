e509b37b38b36c06f54226186fd4a2995521684a
jatannoury <josephtannoury@outlook.com>
2022-07-31 16:32:24 +0300

    ReactJs | add: Add All User Screen

---
+++ admin/src/Screens/AllUsersScreen.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AllUsersScreen = () => {
+  return <div>AllUsersScreen</div>;
+};
+
+export default AllUsersScreen;
--- admin/src/Screens/LandingScreen.js
+++ admin/src/Screens/LandingScreen.js
@@ -1,6 +1,5 @@
 import React from "react";
 import AllBoxes from "../components/AllBoxes";
-import BoxInfo from "../components/BoxInfo";
 import CoinsContainer from "../components/CoinsContainer";
 import NavBar from "../components/NavBar";
 import "../styling/LandingScreen.css";
