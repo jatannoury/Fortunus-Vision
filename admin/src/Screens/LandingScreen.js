10bb2f3c40c587725a9dff6abd99c8450b51b171
jatannoury <josephtannoury@outlook.com>
2022-07-31 15:45:25 +0300

    ReactJs | add: Add total coins container | Landing Screen

--- admin/src/Screens/LandingScreen.js
+++ admin/src/Screens/LandingScreen.js
@@ -3,10 +3,14 @@ import AllBoxes from "../components/AllBoxes";
 import BoxInfo from "../components/BoxInfo";
 import NavBar from "../components/NavBar";
 import "../styling/LandingScreen.css";
+import image1 from "../assets/image1.png";
 const LandingScreen = () => {
   return (
-    <div className="backgroundImage">
+    <div className="backgroundImage1">
       <NavBar />
+      <div className="imageContainer">
+        <img src={image1} alt="" className="image" />
+      </div>
       <AllBoxes />
     </div>
   );
