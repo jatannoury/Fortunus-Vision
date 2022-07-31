e31502836acbac5e004fae3ef3b2444a5bb6dacf
jatannoury <josephtannoury@outlook.com>
2022-07-31 12:18:27 +0300

    ReactJs | add: Add navbar component

--- admin/src/Screens/LandingScreen.js
+++ admin/src/Screens/LandingScreen.js
@@ -1,10 +1,12 @@
 import React from "react";
 import AllBoxes from "../components/AllBoxes";
 import BoxInfo from "../components/BoxInfo";
+import NavBar from "../components/NavBar";
 import "../styling/LandingScreen.css";
 const LandingScreen = () => {
   return (
     <div className="backgroundImage">
+      <NavBar />
       <AllBoxes />
     </div>
   );
---
+++ admin/src/components/NavBar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <div className="NavBarContainer">
+      <span className="title">Fortunus Vision</span>
+    </div>
+  );
+};
+
+export default NavBar;
