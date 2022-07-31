c28f27ab6b9c1d547202835d40b6b2045829eaa5
jatannoury <josephtannoury@outlook.com>
2022-07-31 18:02:04 +0300

    ReactJs | add: Add Navbar component | All USers Screen

--- admin/src/Screens/AllUsersScreen.js
+++ admin/src/Screens/AllUsersScreen.js
@@ -1,7 +1,12 @@
 import React from "react";
+import NavBar from "../components/NavBar";
 import "../styling/AllUsersScreen.css";
 const AllUsersScreen = () => {
-  return <div className="backgroundImage2"></div>;
+  return (
+    <div className="backgroundImage2">
+      <NavBar />
+    </div>
+  );
 };
 
 export default AllUsersScreen;
