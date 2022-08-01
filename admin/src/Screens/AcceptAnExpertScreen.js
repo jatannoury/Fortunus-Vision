23988b19caa38608c766d5d2892f38027870f4eb
jatannoury <josephtannoury@outlook.com>
2022-08-01 03:49:44 +0300

    ReactJs | add: Add expert card | Accept An Expert Screen

--- admin/src/Screens/AcceptAnExpertScreen.js
+++ admin/src/Screens/AcceptAnExpertScreen.js
@@ -1,7 +1,14 @@
 import React from "react";
-
+import NavBar from "../components/NavBar";
+import ProfilePicture from "../components/ProfilePicture";
+import "../styling/AcceptExpert.css";
 const AcceptAnExpertScreen = () => {
-  return <div>AcceptAnExpertScreen</div>;
+  return (
+    <div className="backgroundImage3">
+      <NavBar />
+      <div className="card"></div>
+    </div>
+  );
 };
 
 export default AcceptAnExpertScreen;
