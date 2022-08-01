4444ebd00994f64db2dc3aa9aed641c613c8dff6
jatannoury <josephtannoury@outlook.com>
2022-08-01 13:50:55 +0300

    ReactJs | add: Fetch data from getRequests endpoint | Accept Expert Screen

--- admin/src/Screens/AcceptAnExpertScreen.js
+++ admin/src/Screens/AcceptAnExpertScreen.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NavBar from "../components/NavBar";
 import "../styling/AcceptExpert.css";
 import ExpertCard from "../components/ExpertCard";
+import { getRequests } from "../helper/http";
 const AcceptAnExpertScreen = () => {
+  const [requests, setRequests] = useState([]);
+  useEffect(() => {
+    async function fetchRequests() {
+      let request = await getRequests();
+      setRequests(request);
+    }
+    fetchRequests();
+  }, []);
   return (
     <div className="backgroundImage3">
       <NavBar />
-      <ExpertCard />
+      {requests.length !== 0 &&
+        requests.map((item) => {
+          return <ExpertCard info={item} />;
+        })}
     </div>
   );
 };
