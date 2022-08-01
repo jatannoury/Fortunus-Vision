257c3a14d9c3a1ca9df82919e06cba6c01910175
jatannoury <josephtannoury@outlook.com>
2022-08-01 13:48:39 +0300

    NodeJs | add: Add getRequests route

--- fortunusVision-Backend/src/v1/admin/index.js
+++ fortunusVision-Backend/src/v1/admin/index.js
@@ -1,10 +1,16 @@
 const { Router } = require("express");
-const { getAllUsers, deleteUser, searchUser } = require("./controller");
+const {
+  getAllUsers,
+  deleteUser,
+  searchUser,
+  getRequests,
+} = require("./controller");
 const AdminRouter = Router();
 // const testMiddleware = require("../../middleware/test");
 
 AdminRouter.post("/getAllUsers", getAllUsers);
 AdminRouter.post("/deleteUser", deleteUser);
 AdminRouter.post("/searchUser", searchUser);
+AdminRouter.post("/getRequests", getRequests);
 
 module.exports = AdminRouter;
