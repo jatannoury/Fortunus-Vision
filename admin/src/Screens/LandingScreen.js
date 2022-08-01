10f885a054e636215574852a13f24e1839051300
jatannoury <josephtannoury@outlook.com>
2022-08-01 17:22:16 +0300

    ReactJs | add: get data from fetchData endpoint | Landing Screen

--- admin/src/Screens/LandingScreen.js
+++ admin/src/Screens/LandingScreen.js
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import AllBoxes from "../components/AllBoxes";
 import CoinsContainer from "../components/CoinsContainer";
 import NavBar from "../components/NavBar";
+import { fetchData } from "../helper/http";
 import "../styling/LandingScreen.css";
 const LandingScreen = () => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    async function getData() {
+      let res = await fetchData();
+      setData(res);
+      console.log(res);
+    }
+    getData();
+  }, []);
   return (
     <div className="backgroundImage1">
       <NavBar />
-      <CoinsContainer />
-      <AllBoxes />
+      <CoinsContainer totalCoins={data.totalCoins} />
+      <AllBoxes users={data.usersNb} experts={data.expertsNb} />
     </div>
   );
 };
