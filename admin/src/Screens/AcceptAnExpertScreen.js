d80367a451cefd7382ccfdf2ba8d5452063acf37
jatannoury <josephtannoury@outlook.com>
2022-08-01 03:59:31 +0300

    ReactJs | add: Add expert's info | Accept An Expert Screen

--- admin/src/Screens/AcceptAnExpertScreen.js
+++ admin/src/Screens/AcceptAnExpertScreen.js
@@ -8,6 +8,15 @@ const AcceptAnExpertScreen = () => {
       <NavBar />
       <div className="card">
         <ProfilePicture />
+        <span className="infoText">Joseph Tannoury</span>
+        <span className="infoText">55 years old</span>
+        <span className="infoText">12 years of experience</span>
+        <div className="infoText">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat
+        </div>
       </div>
     </div>
   );
