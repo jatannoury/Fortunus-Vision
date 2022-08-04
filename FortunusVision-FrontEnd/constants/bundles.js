db0bb8813ef85bca167fa9f95d0eb36c22ae8a24
Joseph Tannoury <jatannouy01@gmail.com>
2022-08-04 13:08:52 +0300

    ReactNative | test

--- FortunusVision-FrontEnd/constants/bundles.js
+++
@@ -1,6 +0,0 @@
-export default offers={
-    offerOne:{coinsNumber:100,coinsPrice:10,currency:"$"},
-    offerTwo:{coinsNumber:200,coinsPrice:15,currency:"$"},
-    offerThree:{coinsNumber:300,coinsPrice:30,currency:"$"},
-    offerFour:{coinsNumber:400,coinsPrice:40,currency:"$"},
-}
--- FortunusVision-FrontEnd/screens/CallingScreen.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { View,Text, } from 'react-native'
-const CallingScreen = () => {
-  return (
-    <div>CallingScreen</div>
-  )
-}
-
-export default CallingScreen
