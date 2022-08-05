85e4289dce0c7270350aa73b40ef60412857e093
Joseph Tannoury <jatannouy01@gmail.com>
2022-08-05 16:56:28 +0300

    End | end of the poject

--- fortunusVision_backEnd/index.js
+++ fortunusVision_backEnd/index.js
@@ -13,7 +13,7 @@ mongoose.connect(DB_CONNECT, () => console.log("connected to db"));
 //
 
 //using dependencies
-const hostname = "192.168.1.198";
+const hostname = "192.168.1.198";// Enter your personal hostname fetched from ipconfig
 const portname = 3001;
 const app = express();
 app.use(cors());
